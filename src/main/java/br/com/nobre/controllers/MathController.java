78d068963b90a97feabdb019b5bac26291bc1fb2
unknown <jnobr3c+github@gmail.com>
2026-06-04 09:32:22 -0300

    :sparkles: feat: Implementing exception handling with @ControllerAdvice, @ResponseStatus, and @ExceptionHandler

--- src/main/java/br/com/nobre/controllers/MathController.java
+++ src/main/java/br/com/nobre/controllers/MathController.java
@@ -10,24 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math") // Define o path base para o controlador
 public class MathController {
 
-    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    //http://localhost:8080/math/sum/3/5
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne,
-                      @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
-        }
+                      @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsupportedOperationException("Please set a numeric value!");
+
         return covertToDouble(numberOne) + covertToDouble(numberTwo);
     }
 
-    public static Double covertToDouble(String strNumber) {
-        if (strNumber == null) return 0d;
-        String number = strNumber.replaceAll(",", ".");// Moeda Americana x Brasileira
+    public static Double covertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty())
+            throw new UnsupportedOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",", ".");// Moeda Americana x Brasileira
         return Double.parseDouble(number);
     }
 
     public static boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
