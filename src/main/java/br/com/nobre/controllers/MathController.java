fa8a08c7b0126cf634ffce9186862cea571b426c
unknown <jnobr3c+github@gmail.com>
2026-06-05 09:29:11 -0300

    :recycle: refactor: apply separation of concerns

--- src/main/java/br/com/nobre/controllers/MathController.java
+++ src/main/java/br/com/nobre/controllers/MathController.java
@@ -1,5 +1,7 @@
 package br.com.nobre.controllers;
 
+import br.com.nobre.math.SimpleMath;
+import br.com.nobre.request.converters.NumberConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math") // Define o path base para o controlador
 public class MathController {
 
+    private SimpleMath math = new SimpleMath();
+
     //http://localhost:8080/math/sum/3/5
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedOperationException("Please set a numeric value!");
 
-        return covertToDouble(numberOne) + covertToDouble(numberTwo);
+        return math.sum(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     //http://localhost:8080/math/subtraction/3/5
@@ -26,10 +30,10 @@ public class MathController {
     public Double subtraction(@PathVariable("numberOne") String numberOne,
                       @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedOperationException("Please set a numeric value!");
 
-        return covertToDouble(numberOne) - covertToDouble(numberTwo);
+        return math.subtraction(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     //http://localhost:8080/math/mutiplication/3/5
@@ -37,10 +41,10 @@ public class MathController {
     public Double mutiplication(@PathVariable("numberOne") String numberOne,
                               @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedOperationException("Please set a numeric value!");
 
-        return covertToDouble(numberOne) * covertToDouble(numberTwo);
+        return math.mutiplication(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     //http://localhost:8080/math/division/3/5
@@ -48,10 +52,10 @@ public class MathController {
     public Double division(@PathVariable("numberOne") String numberOne,
                                 @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedOperationException("Please set a numeric value!");
 
-        return covertToDouble(numberOne) / covertToDouble(numberTwo);
+        return math.division(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
     }
 
     //http://localhost:8080/math/mean/3/5 => Média
@@ -59,10 +63,10 @@ public class MathController {
     public Double mean(@PathVariable("numberOne") String numberOne,
                            @PathVariable("numberTwo") String numberTwo
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedOperationException("Please set a numeric value!");
 
-        return (covertToDouble(numberOne) + covertToDouble(numberTwo)) / 2;
+        return math.mean (NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo)) ;
     }
 
     //http://localhost:8080/math/squareRoot/81 => Raiz Quadrada
@@ -70,27 +74,13 @@ public class MathController {
     public Double squareRoot(
             @PathVariable("number") String number
     ) throws Exception {
-        if (!isNumeric(number))
+        if (!NumberConverter.isNumeric(number))
             throw new UnsupportedOperationException("Please set a numeric value!");
         //Double result = Math.sqrt(covertToDouble(number));
         //return result;
-        return Math.sqrt(covertToDouble(number));
+        return math.squareRoot(NumberConverter.covertToDouble(number));
     }
 
-
-
-    public static Double covertToDouble(String strNumber) throws IllegalArgumentException {
-        if (strNumber == null || strNumber.isEmpty())
-            throw new UnsupportedOperationException("Please set a numeric value!");
-        String number = strNumber.replace(",", ".");// Moeda Americana x Brasileira
-        return Double.parseDouble(number);
-    }
-
-    public static boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty()) return false;
-        String number = strNumber.replace(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
 }
 
 
