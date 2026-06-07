d649cc70dc06fc29d3f0f36b9afce4157cf89772
unknown <jnobr3c+github@gmail.com>
2026-06-07 10:34:44 -0300

    :sparkles: feat: Returning lists of objects in JSON

--- src/main/java/br/com/nobre/PersonServices.java
+++ src/main/java/br/com/nobre/PersonServices.java
@@ -1,4 +1,50 @@
 package br.com.nobre;
+import br.com.nobre.model.Person;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
 public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List <Person> findAll() {
+
+        logger.info("Finding all People!");
+
+        List <Person> persons = new ArrayList<Person>();
+        for (int i= 0; i< 8;i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+            return persons;
+    }
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("José");
+        person.setLastName("Nobre");
+        person.setAdress("Marilia - São Paulo - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("First Name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAdress("Some Adress in Brazil");
+        person.setGender("Male");
+        return person;
+    }
 }
--- src/main/java/br/com/nobre/model/Person.java
+++ src/main/java/br/com/nobre/model/Person.java
@@ -1,4 +1,68 @@
 package br.com.nobre.model;
 
-public class Person {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String adress;
+    private String gender;
+
+    public Person() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAdress(), person.getAdress()) && Objects.equals(getGender(), person.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAdress(), getGender());
+    }
 }
