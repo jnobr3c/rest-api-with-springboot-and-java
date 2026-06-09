2db0caa00089961376229beb8b7466f321471324
unknown <jnobr3c+github@gmail.com>
2026-06-09 12:49:10 -0300

    :arrow_up: build: Replacing the service mocks with access to the repository

--- src/main/java/br/com/nobre/controllers/PersonController.java
+++ src/main/java/br/com/nobre/controllers/PersonController.java
@@ -26,7 +26,7 @@ public class PersonController {
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public Person findById(@PathVariable("id") String id) {
+    public Person findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
@@ -56,7 +56,8 @@ public class PersonController {
             method = RequestMethod.DELETE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public void delete(@PathVariable("id") String id) {
+    public void delete(@PathVariable("id") Long id) {
+
         service.delete(id);
     }
 }
--- src/main/java/br/com/nobre/services/PersonServices.java
+++ src/main/java/br/com/nobre/services/PersonServices.java
@@ -1,8 +1,10 @@
 package br.com.nobre.services;
+import br.com.nobre.exception.ResourceNotFoundException;
 import br.com.nobre.model.Person;
+import br.com.nobre.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
@@ -11,58 +13,50 @@ import java.util.logging.Logger;
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
 
     public List <Person> findAll() {
 
         logger.info("Finding all People!");
 
-        List <Person> persons = new ArrayList<Person>();
-        for (int i= 0; i< 8;i++) {
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
-            return persons;
+        return repository.findAll();
     }
 
-    public Person findById(String id) {
+    public Person findById(Long id) {
         logger.info("Finding one Person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("José");
-        person.setLastName("Nobre");
-        person.setAdress("Marilia - São Paulo - Brasil");
-        person.setGender("Male");
-        return person;
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found with ID: " + id));
     }
 
     public Person create(Person person) {
 
         logger.info("Creating one Person!");
 
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person) {
 
         logger.info("Updating one Person!");
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found with ID"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAdress(person.getAdress());
+        entity.setGender(person.getGender());
 
-        return person;
+        return repository.save(person);
     }
 
-    public void delete(String id) {
+    public void delete(Long id) {
         logger.info("Deleting one Person!");
-    }
 
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("First Name " + i);
-        person.setLastName("Last Name " + i);
-        person.setAdress("Some Adress in Brazil");
-        person.setGender("Male");
-        return person;
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found with ID"));
+        repository.delete(entity);
     }
 }
