5d1f8341cf3d15032aa1a3f28e0a23fc3d99ed5b
unknown <jnobr3c+github@gmail.com>
2026-08-10 23:31:32 -0300

    feat(core): Migrations with Flyway

--- src/main/java/br/com/nobre/controllers/TestLogController.java
+++ src/main/java/br/com/nobre/controllers/TestLogController.java
@@ -1,23 +1,25 @@
 package br.com.nobre.controllers;
 
+import br.com.nobre.services.PersonServices;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
-
-    private static final org.slf4j.Logger log = LoggerFactory.getLogger(TestLogController.class);
     private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
 
-    @GetMapping("/test")
-    public String TestLog() {
+    @GetMapping
+    public String testLog() {
         logger.debug("This is an DEBUG log");
         logger.info("This is an INFO log");
         logger.warn("This is an WARN log");
         logger.error("This is an ERROR log");
-        return "Logs generetad sucessfully!";
+        return "Logs generated successfully!";
     }
 }
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'Porbandar - India', 'Indira', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
